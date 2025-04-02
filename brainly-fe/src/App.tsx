75cb43731861dabc8a5eda1a25522a2adb35fb6b
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2025-04-02 10:55:56 +0530

    fe added

---
+++ brainly-fe/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './component/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button variant="primary" size="md" text={"yty"} startIcon={"="} endIcon={"-"} onClick={()=>{}}></Button>
+    </>
+  )
+}
+
+export default App
---
+++ brainly-fe/src/component/ui/button.tsx
@@ -0,0 +1,20 @@
+
+
+export interface ButtonProps {
+    variant:"primary"| "secondary";
+    size:"sm"| "md"|"lg";
+    text:string;
+    startIcon:any;
+    endIcon:any;
+    onClick:()=>void
+}
+
+
+export const Button =(props:ButtonProps)=>{
+  return( 
+   <button>submit </button>
+
+  )
+
+
+}
